f9025842507a42b56ac9ec93685735d0df2a0b08
BaleaCristi <balea.cristian90@gmail.com>
2015-11-04 16:31:14 +0200

    fixed exercise 79

---
+++ MOOC/src/exercise79/Main.java
@@ -0,0 +1,14 @@
+package exercise79;
+
+public class Main {
+	public static void main(String[] args) {
+		NumberStatistics stats = new NumberStatistics();
+		stats.addNumber(3);
+		stats.addNumber(5);
+		stats.addNumber(1);
+		stats.addNumber(2);
+		System.out.println("Amount: " + stats.amountOfNumbers());
+		System.out.println("sum: " + stats.sum());
+		System.out.println("average: " + stats.average());
+	}
+}
--- MOOC/src/exercise79/NumberStatistics.java
+++ MOOC/src/exercise79/NumberStatistics.java
@@ -2,18 +2,31 @@ package exercise79;
 
 public class NumberStatistics {
 
-	int ammountOfNumbers; // tells us how many numbers have been added to the
-							// statistics
+	private int ammountOfNumbers; // tells us how many numbers have been added
+									// to the statistics
+	private int sum;
 
 	public NumberStatistics() {
-		System.out.println("Amount :" + ammountOfNumbers);
+
 	}
 
 	public void addNumber(int number) {
+		this.sum += number;
 		ammountOfNumbers++;
 	}
 
 	public int amountOfNumbers() {
+		return ammountOfNumbers;
 
+	}
+
+	public int sum() {
+		return sum;
+	}
+
+	public double average() {
+
+		double average = sum / (double) ammountOfNumbers;
+		return average;
 	}
 }
