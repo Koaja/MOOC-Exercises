06b6905e59f92669c435d8f2d53aa6c7388e1ae1
BaleaCristi <balea.cristian90@gmail.com>
2015-11-05 17:07:38 +0200

    added random numbers course

---
+++ MOOC/src/course/test.java
@@ -0,0 +1,11 @@
+package course;
+
+import java.util.Random;
+
+public class test {
+	public static void main(String[] args) {
+		Random rng = new Random();
+
+		System.out.println(rng.nextDouble());
+	}
+}
--- MOOC/src/exercise79/Main.java
+++ MOOC/src/exercise79/Main.java
@@ -1,14 +1,34 @@
 package exercise79;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		NumberStatistics stats = new NumberStatistics();
-		stats.addNumber(3);
-		stats.addNumber(5);
-		stats.addNumber(1);
-		stats.addNumber(2);
-		System.out.println("Amount: " + stats.amountOfNumbers());
-		System.out.println("sum: " + stats.sum());
-		System.out.println("average: " + stats.average());
+		NumberStatistics sumAllNumbers = new NumberStatistics();
+		NumberStatistics oddNumbers = new NumberStatistics();
+		NumberStatistics evenNumbers = new NumberStatistics();
+
+		Scanner input = new Scanner(System.in);
+		int number;
+
+		// stats.addNumber(3);
+		// stats.addNumber(5);
+		// stats.addNumber(1);
+		// stats.addNumber(2);
+		// System.out.println("Amount: " + stats.amountOfNumbers());
+		// System.out.println("sum: " + stats.sum());
+		// System.out.println("average: " + stats.average());
+
+		// System.out.println("Enter numbers");
+		// while (true) {
+		// number = input.nextInt();
+		// stats.addNumber(number);
+		// if (number == -1) {
+		// System.out.println(stats.sum());
+		// break;
+		// }
+		// }
+
 	}
 }
